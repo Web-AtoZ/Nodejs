84c8889e248b6215f40c9365d88079f5220f9974
soobinnn <isb9082@naver.com>
2020-05-25 09:23:35 +0900

    feat: 릴리즈 수정, 테스트

--- src/common/components/organisms/Board/BoardDetailContainer.js
+++ src/common/components/organisms/Board/BoardDetailContainer.js
@@ -8,12 +8,14 @@ import {observer} from 'mobx-react';
 import BoardNav from './BoardNav';
 import Button from '@material-ui/core/Button';
 import { useForm } from 'react-hook-form';
-import {useParams} from 'react-router-dom';
+import {useParams, useHistory} from 'react-router-dom';
+import styled from 'styled-components';
 
 const BoardFormContainer = observer(({type}) => {
-  const {boardStore:{board, updateBoard}} = useStores();
+  const {boardStore:{board, updateBoard, save}} = useStores();
   const {register, handleSubmit, errors} =useForm();
   const params = useParams();  
+  const history = useHistory();
   const [isWrite, changeMode] = useState(false);
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
@@ -28,12 +30,18 @@ const BoardFormContainer = observer(({type}) => {
     console.log('hello');
   },[]);
   
-  const onSubmit = useCallback(data => {
+  const onUpdate = useCallback(data => {
     const form = {...board, ...data, content: content}
     updateBoard(params.id, form);
     changeMode(false);
   },[board, content, params.id, updateBoard]);
 
+  const onSave = useCallback(data => {
+    const form = {...data, content: content};
+    save(form)
+      .then(history.push('/boards'))
+      .catch(e=>console.log(e.message));
+  },[content, history, save]);
   
   return (
     <>
@@ -48,17 +56,29 @@ const BoardFormContainer = observer(({type}) => {
         inputRef={register({required: true})}
         InputProps={isWrite ? null : {readOnly: true,}}
         onChange={({target:{value}})=> setTitle(value)}
+        error={errors.title ? true : false}
+        helperText={errors.title ? "Required title" : ""}
       />
       <input type='hidden' name='user_id' ref={register({required:true})} value={1}/>
       <input type='hidden' name='option_id' ref={register({required:true})} value={1}/>
       {isWrite ? <>
-                  <ReactQuill name='content' theme="snow" value={content} onChange={setContent}/>
-                  <Button variant="contained" color="primary" onClick={handleSubmit(onSubmit)}>제출</Button>
+                  <Editor>
+                    <ReactQuill name='content' theme="snow" value={content} onChange={setContent}/>
+                  </Editor>
+                  <Button variant="contained" color="primary" onClick={type==='form' ? handleSubmit(onSave) : handleSubmit(onUpdate)}>제출</Button>
                 </>
-                : <ReactQuill name='content' theme="bubble" value={board.content} readOnly/>
+                : 
+                <Editor>
+                  <ReactQuill name='content' theme="bubble" value={board.content} readOnly/>
+                </Editor>
       }
     </>
   );
 })
 
 export default BoardFormContainer;
+
+const Editor = styled('div')`
+  margin-top: 10px;
+  margin-bottom: 20px;
+`
--- src/pages/Board/templates/BoardTemplate.js
+++ src/pages/Board/templates/BoardTemplate.js
@@ -3,11 +3,12 @@ import Grid from '@material-ui/core/Grid';
 import {observer} from 'mobx-react';
 import BoardListTable from 'common/components/organisms/Board/BoardListTable';
 import BoardAddButton from 'common/components/organisms/Board/BoardAddButton';
-
+import Button from 'common/components/atoms/Button/Button';
 export default observer(function BoardTemplate({boardList}) {
   return (
     <Grid item xs={10}>
       <BoardAddButton/>
+      <Button>test</Button>
       <BoardListTable data={boardList}/>
     </Grid>
   );
