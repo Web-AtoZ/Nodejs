5bf4797e7c7dd173f9de83cfa8c1a7f40f1e982e
MaximSungmo <ksm5318@naver.com>
2020-07-09 01:47:47 +0900

    [임시]

--- src/pages/Restaurant/templates/RestaurantListTemplate.js
+++ src/pages/Restaurant/templates/RestaurantListTemplate.js
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import RestaurantList from '../../../common/components/organisms/Restaurant/RestaurantList'
 import restaurant from '../restaurant.json'
 
+
 const Flats = () => (
     <Grid xs={10}>
         <h1>Restaurant</h1>
