d02a6d0326da7d23841122b889d031a67e453002
soobinnn <isb9082@naver.com>
2020-07-13 09:10:06 +0900

    commit

--- src/pages/Board/index.js
+++ src/pages/Board/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import {Route,Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import BoardContainer from './BoardContainer';
 import BoardDetailContainer from './BoardDetailContainer';
 import BoardFormTemplate from './templates/BoardFormTemplate';
 import LeftSide from 'pages/LeftSide';
-const Board = ({match}) => {
-  return(
-    <>
-      <LeftSide/>
-      <Route exact path={match.path} component={BoardContainer}/>
-      <Switch>
-        <Route path={`${match.path}/form`} component={BoardFormTemplate}/>
-        <Route path={`${match.path}/:id`} component={BoardDetailContainer}/>
-      </Switch>
-    </>
-  );
-}
+const Board = ({ match }) => {
+	return (
+		<>
+			<LeftSide />
+			<Route exact path={match.path} component={BoardContainer} />
+			<Switch>
+				<Route path={`${match.path}/form`} component={BoardFormTemplate} />
+				<Route path={`${match.path}/:id`} component={BoardDetailContainer} />
+			</Switch>
+		</>
+	);
+};
 
 export default Board;
